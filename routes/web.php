52406b6e6f460f053fa0c88b0070757e8f0fd8c5
Muhammed <mohammad.elhagari@gmail.com>
2024-07-05 02:24:55 +0300

    First Step in Laravel11

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
+use App\Models\Models\Teacher;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -23,24 +26,24 @@ Route::post('/student/update/{id}', [StudentController::class, 'update'])->name(
 //Crube operation of teacher
 
 
-Route::get('/teacher', [StudentController::class, 'index'])->name('teacher');
+Route::get('/teacher', [TeacherController::class, 'index'])->name('teacher');
 
-Route::get('/teacher/show/{id}', [StudentController::class, 'show'])->name('teacher.show');
+Route::get('/teacher/show/{id}', [TeacherController::class, 'show'])->name('teacher.show');
 
-Route::get('/teacher/create', [StudentController::class, 'create'])->name('teacher.create');
-Route::get('/teacher/delete/{id}', [StudentController::class, 'delete'])->name('teacher.delete');
-Route::post('/teacher/create', [StudentController::class, 'save'])->name('student.save');
-Route::get('/teacher/edit/{id}', [StudentController::class, 'edit'])->name('teacher.edit');
-Route::post('/teacher/update/{id}', [StudentController::class, 'update'])->name('teacher.update');
+Route::get('/teacher/create', [TeacherController::class, 'create'])->name('teacher.create');
+Route::get('/teacher/delete/{id}', [TeacherController::class, 'delete'])->name('teacher.delete');
+Route::post('/teacher/create', [TeacherController::class, 'save'])->name('student.save');
+Route::get('/teacher/edit/{id}', [TeacherController::class, 'edit'])->name('teacher.edit');
+Route::post('/teacher/update/{id}', [TeacherController::class, 'update'])->name('teacher.update');
 
 
 //Crube operation of course
-Route::get('/course', [StudentController::class, 'index'])->name('course');
+Route::get('/course', [CourseController::class, 'index'])->name('course');
 
-Route::get('/course/show/{id}', [StudentController::class, 'show'])->name('course.show');
+Route::get('/course/show/{id}', [CourseController::class, 'show'])->name('course.show');
 
-Route::get('/course/create', [StudentController::class, 'create'])->name('course.create');
-Route::get('/course/delete/{id}', [StudentController::class, 'delete'])->name('student.delete');
-Route::post('/student/create', [StudentController::class, 'save'])->name('course.save');
-Route::get('/course/edit/{id}', [StudentController::class, 'edit'])->name('course.edit');
-Route::post('/course/update/{id}', [StudentController::class, 'update'])->name('course.update');
+Route::get('/course/create', [CourseController::class, 'create'])->name('course.create');
+Route::get('/course/delete/{id}', [CourseController::class, 'delete'])->name('student.delete');
+Route::post('/student/create', [CourseController::class, 'save'])->name('course.save');
+Route::get('/course/edit/{id}', [CourseController::class, 'edit'])->name('course.edit');
+Route::post('/course/update/{id}', [CourseController::class, 'update'])->name('course.update');
